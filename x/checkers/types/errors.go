b470d58a10c93b726a07726ea0c2b2cf220e77cb
itshaseebsaeed <haseebsyed30000@gmail.com>
2023-06-19 09:44:45 +0500

    save game

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -14,5 +14,4 @@ var (
 	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1103, "black address is invalid: %s")
 	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
 	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
-  )
-  
+)
